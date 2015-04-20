33aee16b0062b02014edec791fc1be78e860e775
Dmitriy Balykin <b_dmitriy82@mail.ru>
2015-04-20 19:49:45 +0300

    Add models

---
+++ TestApp01/Models/Info/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestApp01.Models.Info
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public IDictionary<string, Ownership> Ownerships { get; set; }
+    }
+}
